5d756c880a4972f58f0240c8f90e45d94c0672e1
Liuwen <yuki.liu@lixinchuxing.com>
2024-04-01 20:26:07 +0800

    test

--- src/views/CupboardView/Index.vue
+++ src/views/CupboardView/Index.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="about">
     <h1 class="title-h1">柜子</h1>
-    <img src="@/assets/img/cupboard/1.jpg" />
+    <img src="../assets/img/cupboard/1.jpg" />
     <video controls>
-      <source src="@/assets/img/cupboard/1.mp4" type="video/mp4" />
-      <source src="@/assets/img/cupboard/1.mp4" type="video/ogg" />
+      <source src="../assets/img/cupboard/1.mp4" type="video/mp4" />
+      <source src="../assets/img/cupboard/1.mp4" type="video/ogg" />
       您的浏览器不支持视频标签。
     </video>
-    <img src="@/assets/img/cupboard/2.jpg" />
-    <img src="@/assets/img/cupboard/3.jpg" />
-    <img src="@/assets/img/cupboard/4.jpg" />
+    <img src="../assets/img/cupboard/2.jpg" />
+    <img src="../assets/img/cupboard/3.jpg" />
+    <img src="../assets/img/cupboard/4.jpg" />
   </div>
 </template>
 
